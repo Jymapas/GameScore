21e02e46a281b526489b23b19b5b1f6c5414df46
Jymapas <Jymapas@yandex.ru>
2023-08-27 21:19:19 +0300

    Обновлена логика для получения корректного значения для третьего кейса. При этом добавлена сущность "Frame" и реализован дополнительный проход для учёта бонусов

--- GameScoreCalculator/ScoreCalculator.cs
+++ GameScoreCalculator/ScoreCalculator.cs
@@ -6,21 +6,42 @@
 
         public List<int> ShowScore(IList<string> input)
         {
-            List<int> actualResults = new(input.Count);
+            var actualResults = GetFramesByScore(input);
+
+            AddBonusesToStrikes(actualResults);
+
+            return actualResults.Select(x => x.Score).ToList();
+        }
+
+        private static List<Frame> GetFramesByScore(IList<string> input)
+        {
+            List<Frame> actualResults = new(input.Count);
             for (var i = 0; i < input.Count; i++)
             {
                 var score = input[i].ToUpper();
                 var numScore = ScoreToNumeral(score);
 
-                if (i >= 1 && input[i - 1].Equals(strikeMark, StringComparison.OrdinalIgnoreCase))
+                if (i == 0 || actualResults[^1].IsStrike || actualResults[^1].SecondThrow != null)
                 {
-                    actualResults[^1] += numScore;
+                    actualResults.Add(new Frame(numScore));
+                    continue;
                 }
 
-                actualResults.Add(numScore);
+                actualResults[^1].SecondThrow = numScore;
             }
 
             return actualResults;
+        }
+
+        private static void AddBonusesToStrikes(List<Frame> actualResults)
+        {
+            for (var i = actualResults.Count - 1; i > 0; i--)
+            {
+                if (actualResults[i - 1].IsStrike)
+                {
+                    actualResults[i - 1].Bonus = actualResults[i].Score;
+                }
+            }
         }
 
         private static int ScoreToNumeral(string score)
@@ -29,9 +50,28 @@
             {
                 strikeMark => 10,
                 _ => int.TryParse(score, out var num) && num <= 10 && num >= 0
-                    ? num 
+                    ? num
                     : throw new IncorrectInputException("Incorrect input. Please check your score-list.")
             };
+        }
+
+        private class Frame
+        {
+            public int FirstThrow { get; set; }
+
+            public int? SecondThrow { get; set; }
+
+            public int Bonus { get; set; }
+
+            public bool IsStrike { get; set; }
+
+            public int Score => FirstThrow + (SecondThrow ?? 0) + Bonus;
+
+            public Frame(int firstThrow)
+            {
+                FirstThrow = firstThrow;
+                if (FirstThrow == 10) IsStrike = true;
+            }
         }
     }
 }
