efbd9c63f1c47e1dd473b5fb97b77a3533d6b94e
Jymapas <Jymapas@yandex.ru>
2023-08-27 20:35:19 +0300

    Добавил обработку предыдущего броска, если был страйк

--- GameScoreCalculator/ScoreCalculator.cs
+++ GameScoreCalculator/ScoreCalculator.cs
@@ -2,22 +2,36 @@
 {
     public class ScoreCalculator : IScoreCalculator
     {
+        private const string strikeMark = "X";
+
         public List<int> ShowScore(IList<string> input)
         {
             List<int> actualResults = new(input.Count);
-            foreach (var t in input)
+            for (var i = 0; i < input.Count; i++)
             {
-                var score = t.ToUpper();
-                actualResults.Add(score switch
+                var score = input[i].ToUpper();
+                var numScore = ScoreToNumeral(score);
+
+                if (i >= 1 && input[i - 1].Equals(strikeMark, StringComparison.OrdinalIgnoreCase))
                 {
-                    "X" => 10,
-                    _ => int.TryParse(score, out var numScore) && numScore <= 10 && numScore >= 0
-                        ? numScore 
-                        : throw new IncorrectInputException("Incorrect input. Please check your score-list.")
-                });
+                    actualResults[^1] += numScore;
+                }
+
+                actualResults.Add(numScore);
             }
-            
+
             return actualResults;
+        }
+
+        private static int ScoreToNumeral(string score)
+        {
+            return score switch
+            {
+                strikeMark => 10,
+                _ => int.TryParse(score, out var num) && num <= 10 && num >= 0
+                    ? num 
+                    : throw new IncorrectInputException("Incorrect input. Please check your score-list.")
+            };
         }
     }
 }
